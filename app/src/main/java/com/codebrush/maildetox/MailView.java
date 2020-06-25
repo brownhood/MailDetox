aa6ae704b68372c19cde6ead09befa891c99ddf7
brownhood <tonystark0243@gmail.com>
2020-06-25 14:55:14 +0700

    v1.0

---
+++ app/src/main/java/com/codebrush/maildetox/MailView.java
@@ -0,0 +1,14 @@
+package com.codebrush.maildetox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail_view);
+    }
+}
